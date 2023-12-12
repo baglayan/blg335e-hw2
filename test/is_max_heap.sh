63d571a81da7881c13b6ba4616d437c48fce2c7d
baglayan <baglayan19@itu.edu.tr>
2023-12-12 21:22:41 +0300

    Update is_max_heap test

--- test/is_max_heap.sh
+++ test/is_max_heap.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 filename=$1
+d=$2
 
-if [ -z "$filename" ]; then
-    echo "Usage: $0 <csv_file>"
+# Function to validate if input is a number
+function is_number() {
+    if [[ "$1" =~ ^[1-9]+$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if [ -z "$filename" ] || [ -z "$d" ] || ! is_number "$d" ; then
+    echo "Usage: $0 <csv_file> <d>"
+    echo "<d> should be a positive integer"
     exit 1
 fi
 
@@ -13,30 +24,43 @@ while read -r _ population; do
     data+=("$population")
 done < "$filename"
 
-# Function to check if the array represents a max heap
-function is_max_heap() {
+# Function to check if the array represents a d-ary max heap
+function is_d_ary_max_heap() {
     local n=${#data[@]}
+    local last_parent_index=$((n / d - 1))
     
-    for ((i = 0; i < n / 2; i++)); do
-        left_child=$((2 * i + 1))
-        right_child=$((2 * i + 2))
+    for ((i = last_parent_index; i >= 0; i--)); do
+        current=$i
         
-        # Check if left child exists and is greater than the parent
-        if [ "$left_child" -lt "$n" ] && [ "${data[$left_child]}" -gt "${data[$i]}" ]; then
-            echo "Not a max heap"
-            exit 1
-        fi
-
-        # Check if right child exists and is greater than the parent
-        if [ "$right_child" -lt "$n" ] && [ "${data[$right_child]}" -gt "${data[$i]}" ]; then
-            echo "Not a max heap"
-            exit 1
-        fi
+        while true; do
+            max=$current
+            
+            # Calculate indices of children
+            for ((j = 1; j <= d; j++)); do
+                child=$((d * current + j))
+                if [ "$child" -lt "$n" ] && [ "${data[$child]}" -gt "${data[$max]}" ]; then
+                    max=$child
+                fi
+            done
+            
+            # Check if the current node is less than any of its children
+            if [ "$max" != "$current" ]; then
+                echo "Not a $d-ary max heap"
+                exit 1
+            fi
+            
+            # Move to the next parent
+            current=$((current - 1))
+            
+            if [ "$current" -lt "$((i * d))" ]; then
+                break
+            fi
+        done
     done
     
-    echo "It's a max heap"
+    echo "It's a $d-ary max heap"
     exit 0
 }
 
-# Call the function to check if it's a max heap
-is_max_heap
+# Call the function to check if it's a d-ary max heap
+is_d_ary_max_heap
